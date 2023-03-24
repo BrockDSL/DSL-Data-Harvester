65d40c09107ecf6dea6a123ca809a7543eb416ce
DanielBrett <44175011+DanielBrett@users.noreply.github.com>
2023-03-24 12:03:25 -0400

    Create IDAdder.py

---
+++ IDAdder.py
@@ -0,0 +1,9 @@
+import csv
+
+with open('TWARCResultsNoDuplicates.csv') as inp, open('temp.csv', 'w') as out:
+    reader = csv.reader(inp)
+    writer = csv.writer(out, delimiter=',')
+    #No need to use `insert(), `append()` simply use `+` to concatenate two lists.
+    writer.writerow(['ID'] + next(reader))
+    #Iterate over enumerate object of reader and pass the starting index as 1.
+    writer.writerows([i] + row for i, row in enumerate(reader, 1))
